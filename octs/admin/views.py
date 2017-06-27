aa283dfa4190cf4d53486016c10a1d1ef01f7427
gaoyunchu <1727176361@qq.com>
2017-06-27 14:26:17 +0800

    admin add页面设计和跳转逻辑

--- octs/admin/views.py
+++ octs/admin/views.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for
+from octs.user.models import Term
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin',static_folder='../static')
 
 @blueprint.route('/')
 def home():
     return render_template('admin/index.html')
+@blueprint.route('/term')
+def term():
+    termList = Term.query.order_by(Term.start_time).all()
+    return render_template('admin/term.html', list=termList)
+@blueprint.route('/term/add')
+def term_add():
+
+    return render_template('admin/term/add.html')

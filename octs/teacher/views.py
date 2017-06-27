85f58403189858c93a00f4a46c78c5450ececf5e
chengvvv <582665622@qq.com>
2017-06-27 16:26:44 +0800

    完成了与数据库的连接，显示

--- octs/teacher/views.py
+++ octs/teacher/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, flash, redirect, render_template, request, url_for,sessions
 from octs.user.models import Course
+from .forms import CourseForm
+from octs.database import db
 
 blueprint = Blueprint('teacher', __name__, url_prefix='/teacher',static_folder='../static')
 
@@ -14,11 +16,27 @@ def course():
     return render_template('teacher/course.html', list=courseList)
 
 
-@blueprint.route('/course/edit/<id>')
+@blueprint.route('/course/edit/<id>',methods=['GET','POST'])
 def course_edit(id):
     course = Course.query.filter_by(id=id).first()
+    form = CourseForm()
+    if form.validate_on_submit():
+        course.course_introduction = form.course_introduction.data
+        course.course_outline=form.course_outline.data
+        db.session.add(course)
+        db.session.commit()
+        return redirect(url_for('teacher.course'))
 
-    return render_template('teacher/course_edit.html',course=course)
+    form.coursename.data=course.name
+    form.credit.data=course.credit
+    form.location.data=course.location
+    form.start_time.data=course.start_time
+    form.course_introduction.data=course.course_introduction
+    form.course_outline.data=course.course_outline
+
+
+
+    return render_template('teacher/course_edit.html',form=form)
 
 
 

411407ce6379fcfd890244b2cf839ade6f50c28c
zhangke <kaiueo@outlook.com>
2017-06-28 11:22:10 +0800

    add a is_accepted coloumn in TeamUserRelation

--- octs/message/views.py
+++ octs/message/views.py
@@ -1,10 +1,28 @@
 from flask import Blueprint, flash, redirect, render_template, request, url_for,sessions
-from octs.user.models import Course, Message
+from octs.user.models import Course, Message, User
 from octs.database import db
 
 blueprint = Blueprint('message', __name__, url_prefix='/message',static_folder='../static')
 
 @blueprint.route('/<id>')
 def show_all(id):
-    messages = Message.query.filter_by(to_id=id)
-    return render_template()
+    messages = Message.query.filter_by(to_id=id).all()
+    usernames = []
+    for message in messages:
+        from_id = message.from_id
+        user = User.query.filter_by(id=from_id).first()
+        usernames.append(user.name)
+    length = len(messages)
+    return render_template('message/unread.html', length=length, messages=messages, names=usernames)
+
+@blueprint.route('/<id>')
+def show_unread(id):
+    messages = Message.query.filter_by(to_id=id).all()
+    messages = [message for message in messages if message.has_read==False]
+    usernames = []
+    for message in messages:
+        from_id = message.from_id
+        user = User.query.filter_by(id=from_id).first()
+        usernames.append(user.name)
+    length = len(messages)
+    return render_template('message/unread.html', length=length, messages=messages, names=usernames)

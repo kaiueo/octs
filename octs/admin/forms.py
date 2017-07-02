422d96ce367f6a0aae460bfbdb67a41bbbc296a5
yimo! <1031059881@qq.com>
2017-07-02 12:31:46 +0800

    修改页面大小和位置

--- octs/admin/forms.py
+++ octs/admin/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import PasswordField, StringField,SubmitField,FloatField,DateField, FileField
+from wtforms import PasswordField, StringField,SubmitField,FloatField,DateField, FileField,TextAreaField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 from octs.database import Column, Model, SurrogatePK, db, reference_col, relationship
 from octs.user.models import Term
@@ -8,7 +8,7 @@ class CourseForm(Form):
     coursename = StringField('名称',validators=[DataRequired()])
     credit = FloatField('学分',validators=[DataRequired()])
     location = StringField('地点',validators=[DataRequired()])
-    course_introduction = StringField('课程介绍',validators=[DataRequired()])
+    course_introduction = TextAreaField('课程介绍',validators=[DataRequired()])
     start_time = DateField('开始时间',validators=[DataRequired()])
     submit = SubmitField('提交')
 
